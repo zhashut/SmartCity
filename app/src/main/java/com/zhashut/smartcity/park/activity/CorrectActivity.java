c3e83ff0f7dda5486f2ceb8bca80a4244ae1ac46
2105798885@qq.com <2105798885@qq.com>
2022-12-21 02:26:54 +0800

    v0.6-实现了提交纠错信息接口

---
+++ app/src/main/java/com/zhashut/smartcity/park/activity/CorrectActivity.java
@@ -0,0 +1,101 @@
+package com.zhashut.smartcity.park.activity;
+
+import static com.zhashut.smartcity.park.constant.constant.CORRECT_URL;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zhashut.smartcity.R;
+import com.zhashut.smartcity.common.MessageRes;
+import com.zhashut.smartcity.common.Result;
+import com.zhashut.smartcity.utils.ReqCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class CorrectActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SharedPreferences preferences;
+    private String token;
+    private EditText park_name;
+    private EditText park_spot;
+    private EditText park_content;
+    private EditText park_pho;
+    private EditText park_remark;
+    private Handler handler = MessageRes.ResultHandler(CorrectActivity.this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correct);
+        initView();
+    }
+
+    /**
+     * 初始化界面控件
+     */
+    private void initView() {
+        preferences = getSharedPreferences("config", MODE_PRIVATE);
+        token = preferences.getString("token", "");
+        TextView tv_title = findViewById(R.id.tv_title);
+        tv_title.setText("信息纠错");
+        park_name = findViewById(R.id.park_name);
+        park_spot = findViewById(R.id.park_spot);
+        park_content = findViewById(R.id.park_content);
+        park_pho = findViewById(R.id.park_pho);
+        park_remark = findViewById(R.id.park_remark);
+        findViewById(R.id.btn_park_commit).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_park_commit:
+                String parkName = park_name.getText().toString().trim();
+                String parkSpot = park_spot.getText().toString().trim();
+                String parkContent = park_content.getText().toString().trim();
+                String parkPho = park_pho.getText().toString().trim();
+                String parkRemark = park_remark.getText().toString().trim();
+                if (TextUtils.isEmpty(parkName) || TextUtils.isEmpty(parkSpot) || TextUtils.isEmpty(parkContent)) {
+                    Toast.makeText(this, "请将信息填写完成", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                parkLoading(token, parkName, parkSpot, parkContent, parkPho, parkRemark);
+        }
+    }
+
+    /**
+     * 加载
+     *
+     * @param token       token
+     * @param parkName    停车场名称
+     * @param parkSpot    停车位数
+     * @param parkContent 问题描述
+     * @param parkPho     图片
+     * @param parkRemark  备注
+     */
+    private void parkLoading(String token, String parkName, String parkSpot, String parkContent, String parkPho, String parkRemark) {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("name", parkName);
+            jsonObject.put("spotCount", parkSpot);
+            jsonObject.put("content", parkContent);
+            jsonObject.put("photo", parkPho);
+            jsonObject.put("remark", parkRemark);
+            ReqCallback reqCallback = new ReqCallback();
+            // 回调方法
+            reqCallback.CallBack(CORRECT_URL, token, jsonObject.toString(), handler, Result.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/zhashut/smartcity/park/constant/constant.java
@@ -0,0 +1,17 @@
+package com.zhashut.smartcity.park.constant;
+
+import static com.zhashut.smartcity.common.RequestUrl.REQUEST_URL;
+
+/**
+ * Created with Android Studio.
+ *
+ * @author: 炸薯条
+ * Date: 2022/12/21
+ * Time: 0:44
+ * Description: No Description
+ */
+public class constant {
+    // 提交纠错信息 - POST - 请求头要带 token - application/json
+    public final static String CORRECT_URL = REQUEST_URL + "/prod-api/api/park/correct";
+
+}
