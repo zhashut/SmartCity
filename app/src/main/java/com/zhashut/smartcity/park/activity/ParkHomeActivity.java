16c027ba6744c2d2829b3608bbdf648c6b0c2d1e
2105798885@qq.com <2105798885@qq.com>
2023-01-03 22:59:08 +0800

    v0.1-停哪儿主页面

---
+++ app/src/main/java/com/zhashut/smartcity/park/activity/ParkHomeActivity.java
@@ -0,0 +1,16 @@
+package com.zhashut.smartcity.park.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zhashut.smartcity.R;
+
+public class ParkHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_park_home);
+    }
+}
